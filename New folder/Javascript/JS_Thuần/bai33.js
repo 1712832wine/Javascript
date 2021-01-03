30aa70f186de0188329313ab52fd6c18af6d431a
NGUYỄN HỬU TRÍ <tri.nguyen@techupcorp.com>
2021-01-03 09:18:21 +0700

    V 1.0.1

--- New folder/Javascript/JS_Thuần/bai33.js
+++ New folder/Javascript/JS_Thuần/bai33.js
@@ -6,9 +6,7 @@ function Sort(a) {
     for (let i = 0; i < a.length - 1; i++) {
         for (let j = i + 1; j < a.length; j++) {
             if (a[i] > a[j]) {
-                let temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                [a[i], a[j]] = [a[j], a[i]];
             }
         }
     }
